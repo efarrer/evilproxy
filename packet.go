ccb99dc7b1997f912ca94faf0692b0e87d9b2307
Evan Farrer <efarrer@fusionio.com>
2012-11-30 22:40:09 -0700

    Define a packet
    Since we're going to need to simulate TCP we need to define the basic fields for
    a packet.
    
    The Syn/Ack fields are 64 bits as they don't really take up any bandwidth so
    there is little cost to making them large, and we don't want to have to deal
    with integer rollover.

--- packet.go
+++ packet.go
@@ -1,4 +1,18 @@
 package evil_proxy
 
+type Flags byte
+
+const (
+	None Flags = 0
+	Syn  Flags = 1
+	Ack  Flags = 2
+	Fin  Flags = 4
+)
+
 type Packet struct {
+	Flags      Flags
+	Seq        int64
+	Ack        int64
+	WindowSize int64
+	Payload    []byte
 }

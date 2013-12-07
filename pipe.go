5485a3cc4e9a7ff5417a52031bf27c10bc2b2dff
Evan Farrer <EFarrer@fusionio.com>
2013-12-07 16:30:52 -0700

    Improve documentation and simplify the Pipe interface

--- pipe.go
+++ pipe.go
@@ -1,30 +1,39 @@
 package evil_proxy
 
 type Sender interface {
-	/*
-	 * Queue the packet for sending.
-	 * Panics if the sender is closed.
-	 */
 	Send(*Packet)
-
-	// Closes the Sender
-	Close()
 }
 
 type Receiver interface {
-	/* Receives a packet.
-		 * Waits for a packet if one is not immediately available.
-	     * Returns error if sender has closed.
-	*/
 	Recv() (*Packet, error)
 }
 
+type Closer interface {
+	Close()
+}
+
 /*
- * A pipe is a thread-safe, unidirectional communication channel for
- * transmitting packets data sent with the Sender interface will be available on
- * the pipe's own Receiver interface.
+ * A 'Pipe' is a thread-safe, unidirectional communication channel for
+ * transmitting data. Packet's sent with 'Send' will be available on the pipe's
+ * own 'Recv' interface.
  */
 type Pipe interface {
+	/*
+	 * Queue the 'Packet' for sending.
+	 * Panics if the pipe is closed.
+	 */
 	Sender
+
+	/*
+	 * Closes the pipe.
+	 */
+	Closer
+
+	/*
+	 * Receives a 'Packet'.
+	 * Blocks if a 'Packet' is not immediately available.
+	 * Returns error if the pipe is closed and all queued 'Packet's have been
+	 * received.
+	 */
 	Receiver
 }

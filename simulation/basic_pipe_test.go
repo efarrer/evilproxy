353b04c35d73862ddf5bdbf25ce02ba75781f76c
Evan Farrer <EFarrer@fusionio.com>
2013-12-10 22:28:44 -0700

    Remove benchmark until we come up with a benchmark plan

--- simulation/basic_pipe_test.go
+++ simulation/basic_pipe_test.go
@@ -7,13 +7,3 @@ import (
 func TestPipeBehaviorForBasicPipe(t *testing.T) {
 	PerformPipeTests(func() Pipe { return NewBasicPipe() }, t)
 }
-
-func BenchmarkSendingPacketsOverPipe(b *testing.B) {
-	pkt := &Packet{}
-	p := NewBasicPipe()
-	defer p.Close()
-	for i := 0; i < b.N; i++ {
-		p.Send(pkt)
-		p.Recv()
-	}
-}
